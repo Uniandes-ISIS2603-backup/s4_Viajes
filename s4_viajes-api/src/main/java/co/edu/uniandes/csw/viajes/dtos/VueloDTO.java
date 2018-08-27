a838efd2ffc61a57c787f0a1a6e87d28322b16de
estudiante <estudiante@ISIS2603-332.sis.virtual.uniandes.edu.co>
2018-08-27 13:42:24 -0500

    Se implementa la clase VueloDTO

--- s4_viajes-api/src/main/java/co/edu/uniandes/csw/viajes/dtos/VueloDTO.java
+++ s4_viajes-api/src/main/java/co/edu/uniandes/csw/viajes/dtos/VueloDTO.java
@@ -5,10 +5,77 @@
  */
 package co.edu.uniandes.csw.viajes.dtos;
 
+import java.io.Serializable;
+import java.util.List;
+
 /**
  *
- * @author estudiante
+ * @author Juan Felipe Torres
  */
-public class VueloDTO {
+public class VueloDTO implements Serializable
+{
     
+    //Atributos//
+    
+    private int numero;
+    private double costo;
+    private double puntuacion;
+    private List latLongOrigen;
+    private List latLongDestino;
+    
+    //Constructores//
+    
+    public VueloDTO(){
+        
+    }
+    
+    //Métodos//
+    
+    public int getNumero(){
+        return numero;
+    }
+    
+    public double getCosto(){
+        return costo;
+    }
+    
+    public double getPuntuacion()
+    {
+        return puntuacion;
+    }
+    
+    public List getLatLongOrigen()
+    {
+        return latLongOrigen;
+    }
+    
+    public List getLatLongDestino()
+    {
+        return latLongDestino;
+    }
+    
+    public void setNumero(int pNumero)
+    {
+        numero = pNumero;
+    }
+    
+    public void setCosto(double pCosto)
+    {
+        costo = pCosto;
+    }
+    
+    public void setPuntuacion(double pPuntaje)
+    {
+        puntuacion = pPuntaje;
+    }
+    
+    public void setLatLogOrigen(List pListO)
+    {
+        latLongOrigen = pListO;
+    }
+    
+    public void setLatLongDestino(List pListD)
+    {
+        latLongDestino = pListD;
+    }   
 }
