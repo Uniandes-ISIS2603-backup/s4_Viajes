0fe17d3f05e7b0a093e22fe6a3fc684dbacc2df6
estudiante <estudiante@ISIS2603-330.sis.virtual.uniandes.edu.co>
2018-11-28 19:39:06 -0500

    Correccion de pequenos errores en alojamiento y transporte (BACK).

--- s4_viajes-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ s4_viajes-api/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -35,11 +35,9 @@ public class ApplicationConfig extends Application {
         resources.add(co.edu.uniandes.csw.viajes.resources.ActividadGuiaResource.class);
         resources.add(co.edu.uniandes.csw.viajes.resources.ActividadResource.class);
         resources.add(co.edu.uniandes.csw.viajes.resources.AdministradorResource.class);
-        resources.add(co.edu.uniandes.csw.viajes.resources.AlojamientoProveedorResource.class);
         resources.add(co.edu.uniandes.csw.viajes.resources.AlojamientoResource.class);
         resources.add(co.edu.uniandes.csw.viajes.resources.ComboReservasResourcee.class);
         resources.add(co.edu.uniandes.csw.viajes.resources.ComboResource.class);
-        resources.add(co.edu.uniandes.csw.viajes.resources.ComentarioResource.class);
         resources.add(co.edu.uniandes.csw.viajes.resources.EntradaResource.class);
         resources.add(co.edu.uniandes.csw.viajes.resources.GuiaResource.class);
         resources.add(co.edu.uniandes.csw.viajes.resources.MedallaResource.class);
